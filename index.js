38b6cc2dac57124c8026f35e5df388311e604ab9
chandanch <chandan.cec107@gmail.com>
2017-05-16 19:07:23 +0530

    using global flag added

--- index.js
+++ index.js
@@ -22,3 +22,25 @@ console.log("Using Contructor method",regex1.test(baseString)); // returns true
 // 2. Literal method
 var regex2 = /ll/;
 console.log("Using literal method: ",regex2.test(baseString));
+
+/**
+* Flags in Regex
+*               syntax: 
+*      1. Constructor method:
+*          RegExp(<pattern>, '<flag>')
+*      2. Literal method:
+*          /<pattern>/<flag>
+*
+*               Flags:
+*
+* global(g) - Find all matches rather than stopping at the first match
+* ignorecase(i) - Ignore the case of tbe pattern  
+*/
+
+//Using global flag (g)
+var regexWithGlobalFlag = new RegExp("scr","g");
+console.log(regexWithGlobalFlag.exec(baseString));
+console.log(regexWithGlobalFlag.exec(baseString));
+
+
+
