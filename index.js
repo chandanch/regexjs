ad4eee53c47e12933d3b4436e5a02a2e9d292a48
chandanch <chandan.cec107@gmail.com>
2017-05-16 16:02:24 +0530

    added the contructor method for regex check

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict';
+/**
+*@desc Regular expression in Javascript
+* A regular expression is defined as the sequence of characters that constitute a pattern
+* that can be used to match inside a string
+*/
 
-var hello;
+// Create a string
+var baseString = "Hello this script is all about regular expressions in javascript";
+
+/**
+* A regular expression can be created using two methods
+* 1. Constructor method
+* 2. Literal method
+*/
+
+// 1. Constructor method
+var regex1 = new RegExp('is');
+// check if the pattern is present in the baseString
+console.log(regex1.test(baseString));
+
